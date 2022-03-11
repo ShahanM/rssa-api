30c4267de38ea17caa49999ee63bd78d17fb9c43
Shahan M <iShahanM@gmail.com>
2022-03-11 01:10:47 -0500

    endpoint for communitypref

--- src/app.py
+++ src/app.py
@@ -83,7 +83,13 @@ def show_readme():
 @cross_origin(supports_credentials=True)
 def get_discrete_cont_coupled():
     data = get_discrete_continuous_coupled()
+    
+    dictdata = {movie['item_id']: movie for movie in data}
+    moviedata = movie_db.get_movie_lst(idlist=list(dictdata.keys()))
+    for movie in moviedata:
+        dictdata[movie['movie_id']]['poster'] = movie['poster']
 
+    data = list(dictdata.values())
     return Response(json.dumps(data), mimetype='application/json')
 
 
