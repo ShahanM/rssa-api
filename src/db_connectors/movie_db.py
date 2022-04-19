10004f70fc4b7b2b5fb948c60401f8bc782cc39b
Shahan M <iShahanM@gmail.com>
2022-04-19 19:23:04 -0400

    using a pool of ids instead of querying for random sampling

--- src/db_connectors/movie_db.py
+++ src/db_connectors/movie_db.py
@@ -1,7 +1,7 @@
 import datetime
 import random
+from collections import defaultdict
 from dataclasses import asdict
-import functools
 
 from sqlalchemy import and_
 from sqlalchemy.sql import func
@@ -14,6 +14,21 @@ class MovieDB(object):
 		self.db = db
 		self.parse_datetime = lambda dtstr: datetime.\
 			strptime(dtstr, "%a, %d %b %Y %H:%M:%S %Z")
+
+		self.movie_idx_dict = self._build_hash('./db_connectors/cache/movie_id_lookup.csv')
+		self.iers_movie_idx_dict = self._build_hash('./db_connectors/cache/iers_movie_id_lookup.csv')
+
+	def _build_hash(self, filepath):
+		hashvar = {
+			1: defaultdict(list), 2:defaultdict(list), \
+			3: defaultdict(list), 4: defaultdict(list), 5: defaultdict(list), \
+			6: defaultdict(list)}
+		with open(filepath, 'r') as f:
+			next(f)
+			for line in f:
+				movie_id, rank_group, year_bucket = line.split(',')
+				hashvar[int(rank_group)][int(year_bucket)].append(int(movie_id))
+		return hashvar
 
 	def get_database(self):
 		return self.db
@@ -36,8 +51,7 @@ class MovieDB(object):
 		}
 
 		ers = api == 'ers'
-		seen = Movie.query\
-			.filter(Movie.id.in_([item.item_id for item in seen])).all()
+		seen = [item.item_id for item in seen]
 		seen = tuple() if seen is None else tuple(seen)
 
 		items_to_send = []
@@ -45,80 +59,57 @@ class MovieDB(object):
 		for i in range(num_pages):
 			page_num += i
 			page_num = 5 if page_num > 5 else page_num
+			print(seen)
 			items_to_send.extend(self._generate_page(lim, \
 				sampling_weights=page_dist[page_num], seen=seen, \
 					pagenum=page_num, ers=ers))
-		print(self._generate_page.cache_info())
-		return items_to_send
+			
+		return self.get_movie_from_list(items_to_send, ers)
 
-
-	@functools.lru_cache(maxsize=64)
 	def _generate_page(self,  lim: int, sampling_weights: tuple, seen: tuple, \
 		pagenum:int, ers:bool) -> list:
 		page_items = set()
 		seen = set(seen)
+		idxmap = self.iers_movie_idx_dict if ers else self.movie_idx_dict
+		print(idxmap.keys())
 		print('Building page for user')
 		for group, count in enumerate(sampling_weights, 1):
 			if count == 0: continue
 			random_buckets = random.choices(range(1, 7), \
 				weights=[75, 65, 50, 40, 30, 25], k=count)
 			for bucket in random_buckets:
-				item = None
+				itemidx = None
 				trialcount = 0
-				while item is None:
-					item = self._get_ers_item(group, bucket) \
-						if ers else self._get_rssa_item(group, bucket)
-					if item is None or trialcount > 5:
+				while itemidx is None:
+					if len(idxmap[group][bucket]) > 0:
+						itemidx = random.choice(idxmap[group][bucket])
+					if itemidx is None or trialcount > 5:
 						print('Could not find movie in bucket {} with {} \
 							tries. Moving to next bucket.'\
 								.format(bucket, trialcount))
 						bucket += 1
 						continue
-					if item not in page_items and item not in seen:
-						page_items.add(item)
+					if itemidx not in page_items and itemidx not in seen:
+						page_items.add(itemidx)
 					else:
 						trialcount += 1
 		else:
 			if len(page_items) < lim:
-				excludelst = [itm.id for itm in page_items.union(seen)]
-				items = self._get_ers_filler(excludelst, page_items, lim) \
-					if not ers else self._get_rssa_fillers(excludelst, page_items, lim)
-				page_items.update(items)
-				
-		return self._prep_to_send(page_items, ers)
+				excludelst = page_items.union(seen)
+				while len(page_items) < lim:
+					group, bucket = random.choices(range(2, 7), k=2)
+					if len(idxmap[group][bucket]) > 0:
+						filleritm = random.choice(idxmap[group][bucket])
+						if filleritm not in excludelst:
+							page_items.add(filleritm)
 
-	
+		return page_items
+
 	def get_movie_from_list(self, movieids:list, api:str='rssa') -> list:
 		ers = api == 'ers'
 		movies = Movie.query.filter(Movie.movie_id.in_(movieids)).all()
 
 		return self._prep_to_send(movies, ers)
-
-	def _get_ers_item(self, group, bucket):
-		return Movie.query.filter(and_(\
-					Movie.emotions != None,
-					Movie.emotions.has(MovieEmotions.iers_rank_group==group), \
-					Movie.year_bucket==bucket \
-				))\
-				.order_by(func.random()).limit(1).first()
-
-	def _get_rssa_item(self, group, bucket):
-		return Movie.query.filter_by(rank_group=group, year_bucket=bucket)\
-					.order_by(func.random()).limit(1).first()
-
-	def _get_ers_filler(self, excludelst, page_items, lim):
-		return Movie.query.filter(and_(\
-					Movie.id.notin_(excludelst),
-					Movie.emotions != None
-				))\
-				.order_by(func.random())\
-				.limit(lim - len(page_items)).all()
-
-	def _get_rssa_fillers(self, excludelst, page_items, lim):
-		return Movie.query.filter(Movie.id.notin_(excludelst))\
-					.order_by(func.random())\
-					.limit(lim - len(page_items)).all()
-
 
 	def _prep_to_send(self, movielist, ers):
 		items = []
